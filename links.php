2c0c841c7f2da2e2fad8468d6ef526fccdcfa698
rominou007 <55696244+rominou007@users.noreply.github.com>
2025-06-13 17:51:51 +0100

    initialisation

---
+++ links.php
@@ -0,0 +1,5 @@
+<!-- Bootstrap 5 CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap 5 JS Bundle with Popper (includes popper.js) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
